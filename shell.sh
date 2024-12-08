9fe51730ab88933322d759d4a028e487106fcc26
Keita Kasai <keita.kasai.1230@gmail.com>
2024-12-09 02:37:46 +0900

    環境変数に

--- shell.sh
+++ shell.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -euxo pipefail
 
+PRIVATE_KEY_FILE=~/.ssh/id_rsa
+USER_NAME=isucon
 IP_ADDR=127.0.0.1
+PORT=22
 
-ssh -i ~/.ssh/id_rsa isucon@${IP_ADDR} "sudo cat /var/log/nginx/access.log" > access.log  
-ssh -i ~/.ssh/id_rsa isucon@${IP_ADDR} "sudo cat /var/log/mysql/mysql-slow.log" > mysql-slow.log
+ssh -i ${PRIVATE_KEY_FILE} -p ${PORT} ${USER_NAME}@${IP_ADDR} "sudo cat /var/log/nginx/access.log" > access.log  
+ssh -i ${PRIVATE_KEY_FILE} -p ${PORT} ${USER_NAME}@${IP_ADDR} "sudo cat /var/log/mysql/mysql-slow.log" > mysql-slow.log
 
-ssh -i ~/.ssh/id_rsa isucon@${IP_ADDR} "sudo truncate -s 0 /var/log/nginx/access.log" 
-ssh -i ~/.ssh/id_rsa isucon@${IP_ADDR} "sudo truncate -s 0 /var/log/mysql/mysql-slow.log"
+ssh -i ${PRIVATE_KEY_FILE} -p ${PORT} ${USER_NAME}@${IP_ADDR} "sudo truncate -s 0 /var/log/nginx/access.log" 
+ssh -i ${PRIVATE_KEY_FILE} -p ${PORT} ${USER_NAME}@${IP_ADDR} "sudo truncate -s 0 /var/log/mysql/mysql-slow.log"
 
 make alp file=access.log arg="--sort p99"
 
